4902a4abfacb212d0b30818485b1477371c51c23
Simran Bilkhu <52619680+simb2@users.noreply.github.com>
2026-03-20 13:55:27 -0400

    Update file paths for RDS data loading

--- Simulation_Analysis/performance_measures_ss3.R
+++ Simulation_Analysis/performance_measures_ss3.R
@@ -1,25 +1,25 @@
 # sim study 3 performance measures
 library(tidyverse)
 library(here)
-overfit_post_draws_UGLT <- readRDS(here("Run_Simulations","overfit_post_draws_UGLT.rds"))
-overfit_post_draws_plt_sparse <- readRDS(here("Run_Simulations","overfit_post_draws_plt_sparse.rds"))
+overfit_post_draws_UGLT <- readRDS(here("overfit_post_draws_UGLT.rds"))
+overfit_post_draws_plt_sparse <- readRDS(here("overfit_post_draws_plt_sparse.rds"))
 table(overfit_post_draws_UGLT[[1]]$r)
 table(overfit_post_draws_plt_sparse[[1]]$r)
 
 
-overfit_plt_setting_post_draws_plt_sparse <- readRDS(here("Run_Simulations","overfit_plt_setting_post_draws_plt_sparse.rds"))
-overfit_plt_setting_post_draws_UGLT <- readRDS(here("Run_Simulations","overfit_plt_setting_post_draws_UGLT.rds"))
+overfit_plt_setting_post_draws_plt_sparse <- readRDS(here("overfit_plt_setting_post_draws_plt_sparse.rds"))
+overfit_plt_setting_post_draws_UGLT <- readRDS(here("overfit_plt_setting_post_draws_UGLT.rds"))
 table(overfit_plt_setting_post_draws_plt_sparse[[1]]$r)
 table(overfit_plt_setting_post_draws_UGLT[[1]]$r)
 
 
-overfit_post_draws_10_UGLT <- readRDS(here("Run_Simulations","overfit_post_draws_10_UGLT.rds"))
-overfit_post_draws_plt_10_sparse <- readRDS(here("Run_Simulations","overfit_post_draws_plt_10_sparse.rds"))
+overfit_post_draws_10_UGLT <- readRDS(here("overfit_post_draws_10_UGLT.rds"))
+overfit_post_draws_plt_10_sparse <- readRDS(here("overfit_post_draws_plt_10_sparse.rds"))
 table(overfit_post_draws_10_UGLT[[1]]$r)
 table(overfit_post_draws_plt_10_sparse[[1]]$r)
 
 
-overfit_plt_setting_10_post_draws_UGLT <- readRDS(here("Run_Simulations", "overfit_plt_setting_10_post_draws_UGLT.rds"))
-overfit_plt_setting_10_post_draws_plt_sparse <- readRDS(here("Run_Simulations", "overfit_plt_setting_10_post_draws_plt_sparse.rds"))
+overfit_plt_setting_10_post_draws_UGLT <- readRDS(here("overfit_plt_setting_10_post_draws_UGLT.rds"))
+overfit_plt_setting_10_post_draws_plt_sparse <- readRDS(here("overfit_plt_setting_10_post_draws_plt_sparse.rds"))
 table(overfit_plt_setting_10_post_draws_UGLT[[1]]$r)
 table(overfit_plt_setting_10_post_draws_plt_sparse[[1]]$r)
